d082462734495a382b614e49d4e8d1a8337289a0
Martin Gruber <martin.gruber@email.cz>
2015-06-24 12:24:57 +0200

    Updated: The build process.

--- build.py
+++ build.py
@@ -1,20 +1,23 @@
 # -*- coding: utf-8 -*-
+import os
+
 from pybuilder.core import use_plugin, init, Author
 
 # use_plugin("python.core")
-# use_plugin("python.unittest")
-# use_plugin("python.install_dependencies")
-# use_plugin("python.flake8")
-# use_plugin("python.coverage")
 use_plugin("python.distutils")
-# use_plugin("copy_resources")
+use_plugin("copy_resources")
 
+def read_from(filename):
+    with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), filename)) as f:
+        result = f.read()
+    return result
+
+default_task = ["publish", "clean"]
 
 name = "pybuilder-smart-copy-resources"
 summary = 'PyBuilder plugin for copying additional resources'
 authors = [Author('Martin Gruber', 'martin.gruber@email.cz')]
-version = "0.1.1"
-default_task = "publish"
+version = read_from('VERSION')
 license = 'MIT'
 url = 'https://github.com/margru/pybuilder-smart-copy-resources'
 
@@ -23,7 +26,7 @@ classifiers = [
     #   3 - Alpha
     #   4 - Beta
     #   5 - Production/Stable
-    'Development Status :: 2 - Pre-Alpha',
+    'Development Status :: 3 - Alpha',
 
     # Pick your license as you wish (should match "license" above)
     'License :: OSI Approved :: MIT License',
@@ -41,4 +44,11 @@ Please, see https://github.com/margru/pybuilder-smart-copy-resources for more in
 def set_properties(project):
     project.set_property("name", name)
     project.set_property("version", version)
+
+    build_dir = os.path.join("releases", "$name-$version")
+    project.set_property("dir_dist", build_dir)
     project.set_property("distutils_classifiers", classifiers)
+    project.set_property("distutils_commands", ["sdist", "bdist_egg", "bdist_wheel"])
+
+    project.set_property("copy_resources_glob", ["MANIFEST.in", "VERSION", "README.md", "LICENSE"])
+    project.set_property("copy_resources_target", "${dir_dist}")
